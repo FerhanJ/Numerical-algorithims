4653a43011b0287e37b8a2ec3b4e1db7c775cc36
FerhanJ <32655500+FerhanJ@users.noreply.github.com>
2018-01-29 12:11:57 +0000

    Update markov.py
    Included more comments

--- markov.py
+++ markov.py
@@ -9,12 +9,12 @@ def file2list(filename):
         total_words = total_words + word_list
     return total_words
 
-#get n consecutive words from a list starting from index i in the list
+#string n consecutive words from word_list, a list, starting from index i in the list
 def getWords(word_list, i, n=1): 
-    words = ''
+    words = '' #words is a string to hold our list elements
     for x in range(n):
         if x < n - 1:
-            words += word_list[i + x] + ' '
+            words += word_list[i + x] + ' ' #add the (i + x)th word to our string and repeat n times
         else:
             words += word_list[i + x]
     return words
@@ -22,17 +22,17 @@ def getWords(word_list, i, n=1):
 #takes a list of words and returns a dictionary, d, with prefix keys and n sufixes as values
 def return_suffix(all_words, n): 
     d = {} 
-    i = 0
-    for word in all_words:
+    i = 0 #i is the index of the word after our chosen prefix
+    for word in all_words: #word is our dictionary key, ie our prefix
         i += 1
-        if i <= len(all_words) - n:
-            d.setdefault(word, []).append(getWords(all_words, i, n))
+        if i <= len(all_words) - n: #this stops index out of range errors
+            d.setdefault(word, []).append(getWords(all_words, i, n)) #append a string of n words to our list of suffix values for d[word]
     return d
 
-#takes the dictionary d from previous function, a starting suffix called word and creates a sentence of length n
-def markov(d, word, n=10):
-    print(word, end=' ')
+#takes the dictionary d from previous function, a starting prefix called word and creates a sentence of length n
+def markov(d, prefix, n=10):
+    print(prefix, end=' ') #print our prefix to start the sentence
     for i in range(n):
-        selected = random.choice(d[word])
-        word = selected.split()[-1]
-        print(selected, end=' ')
+        selected = random.choice(d[prefix]) #d[prefix] returns a list of suffixes. This code picks a random value from that list
+        prefix = selected.split()[-1] #our new prefix is the last word of our suffix
+        print(selected, end=' ') #print the selected suffixes
